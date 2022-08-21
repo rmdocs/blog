f48be5fd6b8d049b1744de997f3b07aba018fecd
icbugcoder <icbugcoder@88.com>
2022-08-21 21:16:08 +0800

    Update by the main.sh!

--- main.sh
+++ main.sh
@@ -3,10 +3,10 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+npm run build
+
 git config --global user.email "icbugcoder@88.com"
 git config --global user.name "icbugcoder"
-git init
-git add -A
-
-git commit -m 'To refresh the content!'
-git push git@github.com:rmdocs/full-stack-blog.git
+git add .
+git commit -m 'Update by the main.sh!'
+git push origin
